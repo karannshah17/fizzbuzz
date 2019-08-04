cd3cf6600900b902b7cbf9e7f51bf0a51880dabd
Karan Shah <thebondkaran@gmail.com>
2019-08-04 13:22:34 +0530

    modified unity config  for dependencies

--- FizzBuzz/FizzBuzz.Web/App_Start/UnityConfig.cs
+++ FizzBuzz/FizzBuzz.Web/App_Start/UnityConfig.cs
@@ -1,5 +1,9 @@
+using FizzBuzz.Business.BusinessClasses;
+using FizzBuzz.Business.Core;
+using FizzBuzz.Web.Repository;
 using System.Web.Mvc;
 using Unity;
+using Unity.Injection;
 using Unity.Mvc5;
 
 namespace FizzBuzz.Web
@@ -9,12 +13,28 @@ namespace FizzBuzz.Web
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+            container.RegisterType<IDateTimeProvider, DateProvider>();
+
+            container.RegisterType<INumberModuloCheck, NumberModuloByThreeCheck>("NumberModuloByThree");
+            container.RegisterType<INumberModuloCheck, NumberModuloByFiveCheck>("NumberModuloByFive");
+            container.RegisterType<INumberModuloCheck, NumberModuloByFifteenCheck>("NumberModuloByFifteen");
+            container.RegisterType<INumberModuloCheck, DefaultModuloCheck>("NumericString");
+
+
+            container.RegisterType<IOutputStringService, OutputStringService>(
+     new InjectionConstructor(
+         new ResolvedArrayParameter<INumberModuloCheck>(
+             new ResolvedParameter<INumberModuloCheck>("NumberModuloByFifteen"),
+             new ResolvedParameter<INumberModuloCheck>("NumberModuloByFive"),
+             new ResolvedParameter<INumberModuloCheck>("NumberModuloByThree"),
+           new ResolvedParameter<INumberModuloCheck>("NumericString")
+       )
+     ));
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
