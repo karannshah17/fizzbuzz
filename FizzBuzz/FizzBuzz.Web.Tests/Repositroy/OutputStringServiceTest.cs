aa8526413ec23772845f3b1a7aa517a7ad29684f
Karan Shah <thebondkaran@gmail.com>
2019-08-04 12:52:17 +0530

    Created repository with testcases

---
+++ FizzBuzz/FizzBuzz.Web.Tests/Repositroy/OutputStringServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using FizzBuzz.Business.Core;
+using FizzBuzz.Web.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Web.Tests.Repositroy
+{
+    class OutputStringServiceTest
+    {
+        private INumberModuloCheck[] _NumberModuloCheckProviders;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var mockNumberByFifteenCheck = new Mock<INumberModuloCheck>();
+            var mockNumberByFiveCheck = new Mock<INumberModuloCheck>();
+            var mockNumberByThreeCheck = new Mock<INumberModuloCheck>();
+            var mockDefaultModuloCheck = new Mock<INumberModuloCheck>();
+
+            _NumberModuloCheckProviders = new INumberModuloCheck[] { mockNumberByFifteenCheck.Object,  mockNumberByFiveCheck.Object,
+          mockNumberByThreeCheck.Object,mockDefaultModuloCheck.Object};
+
+        }
+        [Test]
+        [TestCase(2, 1)]
+        [TestCase(4, 3)]
+        [TestCase(6, 5)]
+        public void Check_Output_String_Service_Count(int input, int expectedCount)
+        {
+            IOutputStringService outputStringService = new OutputStringService(_NumberModuloCheckProviders);
+            int ActualCount = outputStringService.getNumber(input).Count;
+            Assert.AreEqual(expectedCount, ActualCount);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.Web/Repository/OutputStringService.cs
@@ -0,0 +1,24 @@
+﻿using FizzBuzz.Business.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FizzBuzz.Web.Repository
+{
+    public class OutputStringService : IOutputStringService
+    {
+        private readonly INumberModuloCheck[] _NumberModuloCheckProviders;
+
+
+        public OutputStringService(INumberModuloCheck[] NumberModuloCheckProviders)
+        {
+            this._NumberModuloCheckProviders = NumberModuloCheckProviders;
+
+        }
+        public List<string> getNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
