b312f626675bd391e206c25198b7941c962578f2
Karan Shah <thebondkaran@gmail.com>
2019-08-04 13:50:36 +0530

    Modified Homecontroller to get final output,Added FizzBuzz

--- FizzBuzz/FizzBuzz.Web/Controllers/HomeController.cs
+++ FizzBuzz/FizzBuzz.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using FizzBuzz.Web.Models;
 using FizzBuzz.Web.Repository;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +28,37 @@ namespace FizzBuzz.Web.Controllers
         [HttpPost]
         public ActionResult Index(NumberViewModel model, int? page)
         {
-            return null;
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+
+            if (ModelState.IsValid)
+            {
+                model.Numbers = GetNumber(model.UserNumber).ToPagedList(pageNumber, pageSize);
+
+                return View("ShowNumber", model);
+            }
+            return View(model);
         }
+        [HttpGet]
+        public ActionResult ShowNumber(int value, int? page)
+        {
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+
+            var model = new NumberViewModel();
+            model.UserNumber = value;
+
+            model.Numbers = GetNumber(value).ToPagedList(pageNumber, pageSize);
+
+            return View(model);
+
+
         }
+        private List<string> GetNumber(int number)
+        {
+
+
+            return _outputStringService.getNumber(number);
+        }
+    }
 }
