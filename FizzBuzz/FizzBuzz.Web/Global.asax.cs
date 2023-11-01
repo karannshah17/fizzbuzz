812a8fe8a5dfa4291f6673bbe98aeb97d70b417c
karannshah17 <Administrator@01QQ9AD6S0CB2>
2023-11-02 01:16:36 +0530

    SAP

--- FizzBuzz/FizzBuzz.Web/Global.asax.cs
+++ FizzBuzz/FizzBuzz.Web/Global.asax.cs
@@ -12,6 +12,7 @@ namespace FizzBuzz.Web
     {
         protected void Application_Start()
         {
+            //SAP
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
